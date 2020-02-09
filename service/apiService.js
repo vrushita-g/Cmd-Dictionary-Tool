87fe8889ef6634e21a10d0fabba228018a1840d5
Vrushita <vrushita.patel1@gmail.com>
2020-02-09 23:21:20 +0530

    Synonyms command added

--- service/apiService.js
+++ service/apiService.js
@@ -1,30 +1,42 @@
-const  RP = require('request-promise');
+const RP = require('request-promise');
 const chalk = require('chalk');
 const CONFIG = require('../config');
 
 let apiService = {
-  displayDefinitions: async function (word) {
-      let apiUrl     = CONFIG.API_URL.BASE_URL +'/word/' +  word + CONFIG.API_URL.DEFINITIONS + CONFIG.API_KEY;
-      let definitions =  await requestApi(apiUrl);
-      if (definitions) {
-          console.log(chalk.green('definitions'.toUpperCase()));
-          for (let definition of definitions) {
-              console.log(chalk.blue(definition.text))
-          }
-      }
-  }
+    displayDefinitions: async function (word) {
+        let apiUrl = CONFIG.API_URL.BASE_URL + '/word/' + word + CONFIG.API_URL.DEFINITIONS + CONFIG.API_KEY;
+        let definitions = await requestApi(apiUrl);
+        if (definitions) {
+            console.log(chalk.green('definitions'.toUpperCase()));
+            for (let definition of definitions) {
+                console.log(chalk.blue(definition.text))
+            }
+        }
+    },
+    displaySynonyms: async function (word) {
+        let apiUrl = CONFIG.API_URL.BASE_URL + '/word/' + word + CONFIG.API_URL.SYNONYMS + CONFIG.API_KEY;
+        let definitions = await requestApi(apiUrl);
+        if (definitions) {
+            console.log(chalk.green('synonyms'.toUpperCase()));
+            for (let synonym of synonyms) {
+                console.log(chalk.blue(synonym.text))
+            }
+        }
+    }
 };
-function requestApi(apiUrl){
-   let options = {
-     method:"GET",
-     url:apiUrl
-   };
-   return RP(options)
+
+function requestApi(apiUrl) {
+    let options = {
+        method: "GET",
+        url: apiUrl
+    };
+    return RP(options)
         .then((response) => {
-           return JSON.parse(response);
+            return JSON.parse(response);
         })
         .catch((err) => {
             console.log(chalk.red(err.body))
         });
 }
+
 module.exports = apiService;
